9342412056aa99584ff4c948bf8145654e3fbcfe
Shyam <67416253+ShyamBhagat2004@users.noreply.github.com>
2024-10-24 00:09:33 -0400

    Time: 39 ms (47.76%), Space: 17.5 MB (57.05%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,27 +9,25 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        def createNode(x: int, next: 'Node' = None, random: 'Node' = None):
-            return Node(x, next, random)
-        myDict = {}
-        curr = head
-        myList = []
-
+        #Edgecase Below
         if not head:
             return None
+        #Define variables
+        myDict = {}
+        myList = []
+        curr = head
 
         while curr:
-            newNode = createNode(curr.val)
-            myDict[curr] = newNode #Map new node to its copy
+            newNode = Node(curr.val)
+            myDict[curr] = newNode
             if newNode.val not in myList:
                 myList.append(newNode)
             curr = curr.next
-
         curr = head
-
-        #Second pass
-        for newNode in myList:
-            newNode.next = myDict.get(curr.next)
-            newNode.random = myDict.get(curr.random)
+        for item in myList:
+            item.next = myDict.get(curr.next)
+            item.random = myDict.get(curr.random)
             curr = curr.next
         return myList[0]
+            
+            
